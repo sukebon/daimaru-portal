77871526303f1b69ea143a7844e37da37846e4a9
sukebon <39957486+sukebon@users.noreply.github.com>
2022-05-23 14:03:31 +0900

    データ入力
    全員分の名前

--- data.js
+++ data.js
@@ -4,6 +4,14 @@ export const users = [
     name: '仁美社長',
   },
   {
+    uid: '',
+    name: '川井専務',
+  },
+  {
+    uid: '',
+    name: '平野常務',
+  },
+  {
     uid: 'MBTOK9Jr0eRWVuoT2YXgZNMoBQH3',
     name: '向井',
   },
@@ -11,12 +19,76 @@ export const users = [
     uid: '6ren6lOJmMbX0mRWjdcpylmATTX2',
     name: '中元部長',
   },
-    {
+  {
     uid: 'q1wpmLlQT8fUCbHyw1BkPuWkSoO2',
     name: '井上係長',
   },
-    {
+  {
     uid: 'BWtKX1VivYUvQJTbmpIiEzR8tSH2',
     name: '池田係長',
+  },
+  {
+    uid: '',
+    name: '丸田部長',
+  },
+  {
+    uid: '',
+    name: '光延次長',
+  },
+  {
+    uid: '',
+    name: '後藤主任',
+  },
+  {
+    uid: '',
+    name: '衣係長',
+  },
+  {
+    uid: '',
+    name: '野田',
+  },
+  {
+    uid: '',
+    name: '中村部長',
+  },
+  {
+    uid: '',
+    name: '経理',
+  },
+  {
+    uid: '',
+    name: '本店',
+  },
+  {
+    uid: '',
+    name: '岩井店長',
+  },
+  {
+    uid: '',
+    name: '鳥岡',
+  },
+  {
+    uid: '',
+    name: '神戸店',
+  },
+  {
+    uid: '',
+    name: '平島センター長',
+  },
+  {
+    uid: '',
+    name: '上野主任',
+  },
+  {
+    uid: '',
+    name: '竹内主任',
+  },
+  {
+    uid: '',
+    name: '藤森工場長代理',
+  },
+  {
+    uid: '',
+    name: '長瀬',
   },
 ];
