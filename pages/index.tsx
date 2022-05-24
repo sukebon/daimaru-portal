24f524821ca56db4cd33cec161286482c0611b14
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-05-24 12:06:04 +0900

    登録画面　変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import Information from '../components/Information';
-import QuickLink from '../components/QuickLink';
-import Slogan from '../components/Slogan';
-import CatalogArea from '../components/CatalogArea';
-import Post from '../components/Post';
-import styles from '../styles/Home.module.css';
-import { Box, Button, Flex, Spacer, Text } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
-import { auth } from '../firebase/auth';
-import { db } from '../firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Information from "../components/Information";
+import QuickLink from "../components/QuickLink";
+import Slogan from "../components/Slogan";
+import CatalogArea from "../components/CatalogArea";
+import Post from "../components/Post";
+import styles from "../styles/Home.module.css";
+import { Box, Button, Flex, Spacer, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { auth } from "../firebase/auth";
+import { db } from "../firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 
 const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
   const router = useRouter();
@@ -21,8 +21,8 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
   const [requests, setRequests] = useState<any>([]);
 
   useEffect(() => {
-    const usersCollectionRef = collection(db, 'requestList');
-    const q = query(usersCollectionRef, orderBy('sendAt', 'desc'));
+    const usersCollectionRef = collection(db, "requestList");
+    const q = query(usersCollectionRef, orderBy("sendAt", "desc"));
     const unsub = onSnapshot(q, (querySnapshot) => {
       setRequests(
         querySnapshot.docs.map((doc) => ({
@@ -36,7 +36,7 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
 
   useEffect(() => {
     if (user === null) {
-      router.push('/login');
+      router.push("/login");
     }
   }, [router, user]);
 
@@ -48,35 +48,33 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
   return (
     <>
       {user && (
-        <div style={{ backgroundColor: '#f7f7f7' }}>
+        <div style={{ backgroundColor: "#f7f7f7" }}>
           <div className={styles.container}>
             <Head>
               <title>大丸白衣ポータル</title>
-              <meta name='description' content='大丸白衣ポータル' />
-              <link rel='icon' href='/favicon.ico' />
+              <meta name="description" content="大丸白衣ポータル" />
+              <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-              <Box w={{ base: '100%', md: '850px' }} mx='auto' py='6'>
-                <Flex justifyContent={'spaceBetween '} alignItems={'center'}>
-                  <Spacer flex={{ base: '0', md: '1' }}></Spacer>
+              <Box w={{ base: "100%", md: "850px" }} mx="auto" py="6">
+                <Flex justifyContent={"spaceBetween "} alignItems={"center"}>
+                  <Spacer flex={{ base: "0", md: "1" }}></Spacer>
                   <Text
-                    flex={{ base: '2', md: '1' }}
-                    fontSize={{ base: 'large', md: '2xl' }}
+                    flex={{ base: "2", md: "1" }}
+                    fontSize={{ base: "large", md: "2xl" }}
                     fontWeight={800}
                   >
                     社内用ポータルサイト
                   </Text>
-                  <Flex flex='1' justifyContent={'end'}>
-                    {user.uid === 'MBTOK9Jr0eRWVuoT2YXgZNMoBQH3' ||
-                    user.uid === 'EVKsigM546MbnakzkDmG0QHlfmn2' ? (
-                      <Link href='./management'>
-                        <a>
-                          <Button marginRight={'10px'}>管理画面 </Button>
-                        </a>
-                      </Link>
-                    ) : (
-                      ' '
-                    )}
+                  <Flex flex="1" justifyContent={"end"}>
+                    {/* {user.uid === "MBTOK9Jr0eRWVuoT2YXgZNMoBQH3" ||
+                      (user.uid === "EVKsigM546MbnakzkDmG0QHlfmn2" && ( */}
+                    <Link href="./management">
+                      <a>
+                        <Button marginRight={"10px"}>依頼する </Button>
+                      </a>
+                    </Link>
+                    {/* ))} */}
                     <Button onClick={logout}>ログアウト</Button>
                   </Flex>
                 </Flex>
@@ -84,23 +82,23 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
                 <Information news={newsData.contents} />
                 <QuickLink link={linkData.contents} />
                 <CatalogArea />
-                {user.uid !== 'fIwZyubjTfgr0lbRb9VjIkOjKTB2' ? (
+                {user.uid !== "fIwZyubjTfgr0lbRb9VjIkOjKTB2" ? (
                   <Box
-                    margin={'20px 0'}
-                    padding={'20px'}
-                    border='1px'
-                    borderColor={'gray.200'}
-                    borderRadius={'lg'}
-                    backgroundColor={'white'}
+                    margin={"20px 0"}
+                    padding={"20px"}
+                    border="1px"
+                    borderColor={"gray.200"}
+                    borderRadius={"lg"}
+                    backgroundColor={"white"}
                   >
-                    <Text fontSize='2xl' mt='1' ml='1'>
+                    <Text fontSize="2xl" mt="1" ml="1">
                       お手伝い依頼一覧
                     </Text>
 
                     <Post requests={requests} />
                   </Box>
                 ) : (
-                  ''
+                  ""
                 )}
               </Box>
             </main>
@@ -115,10 +113,10 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
 export default Home;
 
 export async function getStaticProps() {
-  const accessPoint = 'https://portal-site.microcms.io/api/v1';
+  const accessPoint = "https://portal-site.microcms.io/api/v1";
   const options = {
     headers: {
-      'X-MICROCMS-API-KEY': '5c23d3e8eaa0448388ca527e0e00c829611f',
+      "X-MICROCMS-API-KEY": "5c23d3e8eaa0448388ca527e0e00c829611f",
     },
   };
   const sloganRes = await fetch(`${accessPoint}/slogan`, options);
--- pages/management.tsx
+++ pages/management.tsx
@@ -1,19 +1,19 @@
-import { Box, Flex } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { db } from '../firebase/auth';
-import { auth } from '../firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Form from '../components/Form';
-import PostManagement from '../components/PostManagement';
+import { Box, Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase/auth";
+import { auth } from "../firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Form from "../components/Form";
+import PostManagement from "../components/PostManagement";
 
 const Management = () => {
   const [user] = useAuthState(auth);
   const [requests, setRequests] = useState<any>([]);
 
   useEffect(() => {
-    const usersCollectionRef = collection(db, 'requestList');
-    const q = query(usersCollectionRef, orderBy('sendAt', 'desc'));
+    const usersCollectionRef = collection(db, "requestList");
+    const q = query(usersCollectionRef, orderBy("sendAt", "desc"));
     const unsub = onSnapshot(q, (querySnapshot) => {
       setRequests(
         querySnapshot.docs.map((doc) => ({
@@ -27,26 +27,26 @@ const Management = () => {
 
   return (
     <>
-      {user?.uid === 'MBTOK9Jr0eRWVuoT2YXgZNMoBQH3' ||
-      user?.uid === 'EVKsigM546MbnakzkDmG0QHlfmn2' ? (
+      {user && (
         <Box
-          width={'100%'}
-          backgroundColor={'#f7f7f7'}
-          paddingBottom={'50px'}
-          minH={'100vh'}
+          width={"100%"}
+          backgroundColor={"#f7f7f7"}
+          paddingBottom={"50px"}
+          minH={"100vh"}
         >
           <Flex
-            flexDirection={'column'}
-            alignItems={'center'}
-            width={{ md: '800px' }}
-            margin={'0 auto'}
+            flexDirection={"column"}
+            alignItems={"center"}
+            width={{ md: "800px" }}
+            margin={"0 auto"}
           >
             <Form />
-            <PostManagement requests={requests} />
+            {user?.uid === "MBTOK9Jr0eRWVuoT2YXgZNMoBQH3" ||
+              (user?.uid === "EVKsigM546MbnakzkDmG0QHlfmn2" && (
+                <PostManagement requests={requests} />
+              ))}
           </Flex>
         </Box>
-      ) : (
-        ''
       )}
     </>
   );
