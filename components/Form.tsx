70579d854d0639d9e51f87927467aff2691f6249
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-05-24 09:50:35 +0900

    edit ボタン　切り替え

--- components/Form.tsx
+++ components/Form.tsx
@@ -13,30 +13,30 @@ import {
   NumberInputStepper,
   Select,
   Textarea,
-} from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { db, auth } from '../firebase/auth';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import Link from 'next/link';
-import { dateTime } from '../date';
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { db, auth } from "../firebase/auth";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import Link from "next/link";
+import { dateTime } from "../date";
 
 const Form = () => {
   const [user] = useAuthState(auth);
-  const [title, setTitle] = useState('');
-  const [startDay, setStartDay] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endDay, setEndDay] = useState('');
-  const [endTime, setEndTime] = useState('');
-  const [applicant, setApplicant] = useState('1');
-  const [person, setPerson] = useState('');
-  const [moreless, setMoreless] = useState('');
-  const [level, setLevel] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [startDay, setStartDay] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endDay, setEndDay] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [applicant, setApplicant] = useState("1");
+  const [person, setPerson] = useState("");
+  const [moreless, setMoreless] = useState("");
+  const [level, setLevel] = useState("");
+  const [content, setContent] = useState("");
 
   const addRequest = async () => {
     try {
-      const docRef = await addDoc(collection(db, 'requestList'), {
+      const docRef = await addDoc(collection(db, "requestList"), {
         title: title,
         startDay: startDay,
         startTime: startTime,
@@ -53,19 +53,19 @@ const Form = () => {
         editAt: false,
         sendAt: serverTimestamp(),
       });
-      console.log('Document written with ID: ', docRef.id);
-      setTitle('');
-      setStartDay('');
-      setStartTime('');
-      setEndDay('');
-      setEndTime('');
-      setApplicant('');
-      setPerson('');
-      setMoreless('');
-      setLevel('');
-      setContent('');
+      console.log("Document written with ID: ", docRef.id);
+      setTitle("");
+      setStartDay("");
+      setStartTime("");
+      setEndDay("");
+      setEndTime("");
+      setApplicant("");
+      setPerson("");
+      setMoreless("");
+      setLevel("");
+      setContent("");
     } catch (e) {
-      console.error('Error adding document: ', e);
+      console.error("Error adding document: ", e);
     }
   };
 
@@ -73,60 +73,60 @@ const Form = () => {
     <>
       {user && (
         <Flex
-          flexDirection='column'
-          backgroundColor='#f7f7f7'
-          alignItems='center'
-          padding={'0'}
-          width={'100%'}
+          flexDirection="column"
+          backgroundColor="#f7f7f7"
+          alignItems="center"
+          padding={"0"}
+          width={"100%"}
         >
           <Box
-            minW={{ base: '90%', md: '800px' }}
-            marginTop={'50px'}
-            marginBottom={'50px'}
+            minW={{ base: "90%", md: "800px" }}
+            marginTop={"50px"}
+            marginBottom={"50px"}
           >
             <Flex
-              alignItems={'center'}
-              justifyContent={'space-between'}
-              marginBottom={'20px'}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              marginBottom={"20px"}
             >
-              <Heading color='teal.400'>お手伝い依頼</Heading>
-              <Link href='./'>
+              <Heading color="teal.400">お手伝い依頼</Heading>
+              <Link href="./">
                 <a>
                   <Button>トップへ戻る</Button>
                 </a>
               </Link>
             </Flex>
             <FormControl>
-              <FormLabel htmlFor='title'>タイトル</FormLabel>
+              <FormLabel htmlFor="title">タイトル</FormLabel>
               <Input
-                id='title'
-                type='text'
+                id="title"
+                type="text"
                 value={title}
-                placeholder='タイトルを入力してください。'
-                backgroundColor={'white'}
-                marginBottom={'20px'}
+                placeholder="タイトルを入力してください。"
+                backgroundColor={"white"}
+                marginBottom={"20px"}
                 onChange={(e) => setTitle(e.target.value)}
               />
-              <Flex flexDirection={{ base: 'column', md: 'row' }}>
-                <Flex flex={'1'}>
-                  <Box flex={'1'} marginRight={'5px'}>
-                    <FormLabel htmlFor='startDay'>開始時刻</FormLabel>
+              <Flex flexDirection={{ base: "column", md: "row" }}>
+                <Flex flex={"1"}>
+                  <Box flex={"1"} marginRight={"5px"}>
+                    <FormLabel htmlFor="startDay">開始時刻</FormLabel>
                     <Input
-                      id='startDay'
-                      type='date'
+                      id="startDay"
+                      type="date"
                       value={startDay}
-                      placeholder='開始時刻'
-                      backgroundColor={'white'}
-                      marginBottom={'20px'}
+                      placeholder="開始時刻"
+                      backgroundColor={"white"}
+                      marginBottom={"20px"}
                       onChange={(e) => setStartDay(e.target.value)}
                     />
                   </Box>
-                  <Box flex={'1'} marginRight={{ base: '0', md: '5px' }}>
-                    <FormLabel htmlFor='startTime'>　</FormLabel>
+                  <Box flex={"1"} marginRight={{ base: "0", md: "5px" }}>
+                    <FormLabel htmlFor="startTime">　</FormLabel>
                     <Select
                       value={startTime}
-                      placeholder='---'
-                      backgroundColor={'white'}
+                      placeholder="---"
+                      backgroundColor={"white"}
                       onChange={(e) => setStartTime(e.target.value)}
                     >
                       {dateTime.map((d, index) => (
@@ -137,25 +137,25 @@ const Form = () => {
                     </Select>
                   </Box>
                 </Flex>
-                <Flex flex={'1'}>
-                  <Box flex={'1'} marginRight={'5px'}>
-                    <FormLabel htmlFor='endDay'>終了時刻</FormLabel>
+                <Flex flex={"1"}>
+                  <Box flex={"1"} marginRight={"5px"}>
+                    <FormLabel htmlFor="endDay">終了時刻</FormLabel>
                     <Input
-                      id='endDay'
-                      type='date'
+                      id="endDay"
+                      type="date"
                       value={endDay}
-                      placeholder='終了時刻'
-                      backgroundColor={'white'}
-                      marginBottom={'20px'}
+                      placeholder="終了時刻"
+                      backgroundColor={"white"}
+                      marginBottom={"20px"}
                       onChange={(e) => setEndDay(e.target.value)}
                     />
                   </Box>
-                  <Box flex={'1'} marginRight={{ base: '0', md: '5px' }}>
-                    <FormLabel htmlFor='endTime'>　</FormLabel>
+                  <Box flex={"1"} marginRight={{ base: "0", md: "5px" }}>
+                    <FormLabel htmlFor="endTime">　</FormLabel>
                     <Select
                       value={endTime}
-                      placeholder='---'
-                      backgroundColor={'white'}
+                      placeholder="---"
+                      backgroundColor={"white"}
                       onChange={(e) => setEndTime(e.target.value)}
                     >
                       {dateTime.map((d, index) => (
@@ -167,45 +167,45 @@ const Form = () => {
                   </Box>
                 </Flex>
               </Flex>
-              <Flex flexDirection={{ base: 'column', md: 'row' }}>
-                <Flex flex={'1'}>
-                  <Box flex={'1'} marginRight={'5px'}>
-                    <FormLabel htmlFor='person'>タスク責任者</FormLabel>
+              <Flex flexDirection={{ base: "column", md: "row" }}>
+                <Flex flex={"1"}>
+                  <Box flex={"1"} marginRight={"5px"}>
+                    <FormLabel htmlFor="person">タスク責任者</FormLabel>
                     <Input
-                      id='person'
-                      type='string'
+                      id="person"
+                      type="string"
                       value={person}
-                      placeholder='タスク責任者'
-                      backgroundColor={'white'}
-                      marginBottom={'20px'}
+                      placeholder="タスク責任者"
+                      backgroundColor={"white"}
+                      marginBottom={"20px"}
                       onChange={(e) => setPerson(e.target.value)}
                     />
                   </Box>
-                  <Box flex={'1'} marginRight={{ base: '0', md: '5px' }}>
-                    <FormLabel htmlFor='level'>レベル</FormLabel>
+                  <Box flex={"1"} marginRight={{ base: "0", md: "5px" }}>
+                    <FormLabel htmlFor="level">レベル</FormLabel>
                     <Select
                       value={level}
-                      placeholder='---'
-                      backgroundColor={'white'}
+                      placeholder="---"
+                      backgroundColor={"white"}
                       onChange={(e) => setLevel(e.target.value)}
                     >
-                      <option value='★★★'>★★★</option>
-                      <option value='★★'>★★</option>
-                      <option value='★'>★</option>
+                      <option value="3">★★★</option>
+                      <option value="2">★★</option>
+                      <option value="1">★</option>
                     </Select>
                   </Box>
                 </Flex>
-                <Flex flex={'1'}>
+                <Flex flex={"1"}>
                   <Box
-                    flex={'1'}
-                    marginRight={'5px'}
-                    marginBottom={{ base: '20px', md: '0' }}
+                    flex={"1"}
+                    marginRight={"5px"}
+                    marginBottom={{ base: "20px", md: "0" }}
                   >
-                    <FormLabel htmlFor='applicant'>募集人数</FormLabel>
+                    <FormLabel htmlFor="applicant">募集人数</FormLabel>
                     <NumberInput
                       value={applicant}
-                      placeholder='募集人数'
-                      backgroundColor={'white'}
+                      placeholder="募集人数"
+                      backgroundColor={"white"}
                       onChange={(e) => setApplicant(e)}
                     >
                       <NumberInputField />
@@ -215,37 +215,37 @@ const Form = () => {
                       </NumberInputStepper>
                     </NumberInput>
                   </Box>
-                  <Box flex={'1'} marginRight={{ base: '0', md: '5px' }}>
-                    <FormLabel htmlFor='moreless'>　</FormLabel>
+                  <Box flex={"1"} marginRight={{ base: "0", md: "5px" }}>
+                    <FormLabel htmlFor="moreless">　</FormLabel>
                     <Select
                       value={moreless}
-                      placeholder='---'
-                      backgroundColor={'white'}
+                      placeholder="---"
+                      backgroundColor={"white"}
                       onChange={(e) => setMoreless(e.target.value)}
                     >
-                      <option value='以上'>以上</option>
-                      <option value='まで'>まで</option>
+                      <option value="以上">以上</option>
+                      <option value="まで">まで</option>
                     </Select>
                   </Box>
                 </Flex>
               </Flex>
-              <FormLabel htmlFor='content'>内容</FormLabel>
+              <FormLabel htmlFor="content">内容</FormLabel>
               <Textarea
-                id='content'
+                id="content"
                 value={content}
-                placeholder='内容を入力してください。'
-                backgroundColor={'white'}
-                marginBottom={'20px'}
+                placeholder="内容を入力してください。"
+                backgroundColor={"white"}
+                marginBottom={"20px"}
                 onChange={(e) => setContent(e.target.value)}
               />
             </FormControl>
             <Button
               borderRadius={0}
-              type='submit'
-              variant='solid'
-              colorScheme='teal'
-              width='full'
-              rounded='5'
+              type="submit"
+              variant="solid"
+              colorScheme="teal"
+              width="full"
+              rounded="5"
               onClick={addRequest}
               disabled={title && content && person ? false : true}
             >
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,16 +1,17 @@
-import { Box, Button, Flex, Heading, Spacer, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 import {
   arrayRemove,
   arrayUnion,
   doc,
   setDoc,
   updateDoc,
-} from 'firebase/firestore';
-import { NextPage } from 'next';
-import React, { useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { db, auth } from '../firebase/auth';
-import { users } from '../data.js';
+} from "firebase/firestore";
+import { NextPage } from "next";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { db, auth } from "../firebase/auth";
+import { users } from "../data.js";
+import { starLevel, dayOfWeek } from "../functions.js";
 
 interface Props {
   requests: {
@@ -36,81 +37,75 @@ const Post: NextPage<Props> = ({ requests }) => {
   const [user] = useAuthState(auth);
 
   //参加する
-  const setRequest = async (uid: any) => {
-    const docRef = doc(db, 'requestList', uid);
+  const setRequest = async (uid: string) => {
+    const docRef = doc(db, "requestList", uid);
     await updateDoc(docRef, {
       member: arrayUnion(user && user.uid),
     });
   };
 
   //参加を取り消す
-  const removeRequest = async (uid: any) => {
-    const docRef = doc(db, 'requestList', uid);
+  const removeRequest = async (uid: string) => {
+    const docRef = doc(db, "requestList", uid);
     await updateDoc(docRef, {
       member: arrayRemove(user && user.uid),
     });
   };
 
-  const dayOfWeek = (d: string) => {
-    let date = new Date(d);
-    let number = date.getDay();
-    return ['日', '月', '火', '水', '木', '金', '土'][number];
-  };
-
   return (
     <>
       {requests.map((request: any) => (
-        <Box key={request.id} style={{ width: '100%' }}>
+        <Box key={request.id} style={{ width: "100%" }}>
           {request.displayAt === true && request.deleteAt === false ? (
             <Box
-              maxW='sm'
-              borderTop='none'
-              overflow='hidden'
-              margin={'0 auto 0'}
-              padding={'20px 0 10px'}
-              minW={{ base: '100%' }}
-              backgroundColor={'white'}
+              maxW="sm"
+              borderTop="none"
+              overflow="hidden"
+              margin={"0 auto 0"}
+              padding={"20px 0 10px"}
+              minW={{ base: "100%" }}
+              backgroundColor={"white"}
             >
-              <Text fontSize={'2xl'} paddingBottom={'5px'}>
-                {request.level}{' '}
+              <Text fontSize={"2xl"} paddingBottom={"5px"}>
+                {starLevel(request.level)}{" "}
               </Text>
-              <Heading fontSize={'2xl'} paddingBottom={'10px'}>
+              <Heading fontSize={"2xl"} paddingBottom={"10px"}>
                 {request.title}
               </Heading>
-              <Flex flexDirection={{ base: 'column', md: 'row' }}>
-                <Text marginRight={'10px'}>
+              <Flex flexDirection={{ base: "column", md: "row" }}>
+                <Text marginRight={"10px"}>
                   【開始】{request.startDay}
                   {dayOfWeek(request.startDay)}-{request.startTime}
                 </Text>
-                <Text marginRight={'10px'}>
+                <Text marginRight={"10px"}>
                   【終了】{request.endDay}
                   {dayOfWeek(request.endDay)}-{request.endTime}
                 </Text>
-                <Text marginRight={'10px'}>
+                <Text marginRight={"10px"}>
                   【募集人数】{request.applicant}人{request.moreless}
                 </Text>
                 <Text>【責任者】{request.person}</Text>
               </Flex>
-              <Text padding={'10px 0'}>{request.content}</Text>
+              <Text padding={"10px 0"}>{request.content}</Text>
 
               <Flex
-                justifyContent={'space-between'}
-                alignItems={'center'}
-                marginTop={'10px'}
-                padding={'5px 0 10px'}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                marginTop={"10px"}
+                padding={"5px 0 10px"}
               >
-                <Flex flexWrap={'wrap'}>
+                <Flex flexWrap={"wrap"}>
                   {users.map((user: any) => (
                     <Box
                       key={user.uid}
-                      padding={'5px'}
-                      margin={'10px 10px 10px 0'}
-                      borderRadius={'lg'}
-                      backgroundColor={'gray.500'}
-                      color={'white'}
-                      fontSize={{ base: 'sm' }}
+                      padding={"5px"}
+                      margin={"10px 10px 10px 0"}
+                      borderRadius={"lg"}
+                      backgroundColor={"gray.500"}
+                      color={"white"}
+                      fontSize={{ base: "sm" }}
                       display={
-                        !request.member.includes(user.uid) ? 'none' : 'block'
+                        !request.member.includes(user.uid) ? "none" : "block"
                       }
                     >
                       {request.member.includes(user.uid) && user.name}
@@ -120,22 +115,22 @@ const Post: NextPage<Props> = ({ requests }) => {
                 {request.member.includes(user?.uid) ? (
                   <Button
                     onClick={() => removeRequest(request.id)}
-                    color='white'
-                    bg='#17a6ca'
-                    _hover={{ bg: '#17a6ca' }}
-                    _focus={{ outline: 'none' }}
-                    fontSize={{ base: 'sm' }}
+                    color="white"
+                    bg="#17a6ca"
+                    _hover={{ bg: "#17a6ca" }}
+                    _focus={{ outline: "none" }}
+                    fontSize={{ base: "sm" }}
                   >
                     参加を取り消す
                   </Button>
                 ) : (
                   <Button
                     onClick={() => setRequest(request.id)}
-                    color='white'
-                    bg='orange'
-                    _hover={{ bg: '##orange' }}
-                    _focus={{ outline: 'none' }}
-                    fontSize={{ base: 'sm' }}
+                    color="white"
+                    bg="orange"
+                    _hover={{ bg: "##orange" }}
+                    _focus={{ outline: "none" }}
+                    fontSize={{ base: "sm" }}
                   >
                     参加する
                   </Button>
@@ -144,7 +139,7 @@ const Post: NextPage<Props> = ({ requests }) => {
               <hr />
             </Box>
           ) : (
-            ''
+            ""
           )}
         </Box>
       ))}
