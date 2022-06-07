3649c431ce35355cfe52928b8136bb0a49b25a21
sukebon <39957486+sukebon@users.noreply.github.com>
2022-06-08 00:07:48 +0900

    クレーム報告書　html　８０％

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,12 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 import HeaderMenuButton from './HeaderMenuButton';
+import { Users } from '../data';
+import { authState } from '../store/authState';
+import { useRecoilValue } from 'recoil';
 
 const Header = () => {
+  const currentUser = useRecoilValue(authState);
   return (
     <>
       <Flex
@@ -15,17 +19,23 @@ const Header = () => {
         alignItems={'center'}
         backgroundColor={'#38b2ac'}
       >
-        <Link href='./'>
-          <a>
-            <Text
-              color={'white'}
-              fontSize={{ base: 'large', md: '2xl' }}
-              fontWeight={'bold'}
-            >
-              社内用ポータルサイト
-            </Text>
-          </a>
-        </Link>
+        <Flex alignItems={'center'} color={'white'}>
+          <Link href='./'>
+            <a>
+              <Text
+                color={'white'}
+                fontSize={{ base: 'large', md: '2xl' }}
+                fontWeight={'bold'}
+              >
+                社内用ポータルサイト
+              </Text>
+            </a>
+          </Link>
+          {/* <Text>
+            ログイン：
+            {Users.map((user) => user.uid === currentUser && user.name)}
+          </Text> */}
+        </Flex>
         <HeaderMenuButton />
       </Flex>
     </>
