13afe6b8a62c7c10c512a6ef1e513ba7bff52cd4
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-06-08 16:41:33 +0900

    claim page

--- components/HeaderMenuButton.tsx
+++ components/HeaderMenuButton.tsx
@@ -6,12 +6,12 @@ import {
   MenuGroup,
   MenuItem,
   MenuList,
-} from '@chakra-ui/react';
-import Link from 'next/link';
-import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { auth } from '../firebase/auth';
-import { authState } from '../store/authState';
+} from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { auth } from "../firebase/auth";
+import { authState } from "../store/authState";
 
 const HeaderMenuButton = () => {
   const currentUser = useRecoilValue(authState);
@@ -23,29 +23,29 @@ const HeaderMenuButton = () => {
 
   return (
     <Menu>
-      <MenuButton as={Button} colore='#241749'>
+      <MenuButton as={Button} colore="#241749">
         Menu
       </MenuButton>
       <MenuList>
-        <Link href='./'>
+        <Link href="/">
           <a>
             <MenuItem>トップページ</MenuItem>
           </a>
         </Link>
-        <Link href='./recruitment'>
+        <Link href="/recruitment">
           <a>
             <MenuItem>お手伝い依頼</MenuItem>
           </a>
         </Link>
-        {currentUser === 'MBTOK9Jr0eRWVuoT2YXgZNMoBQH3' ||
-        currentUser === 'EVKsigM546MbnakzkDmG0QHlfmn2' ? (
-          <Link href='./claim'>
+        {currentUser === "MBTOK9Jr0eRWVuoT2YXgZNMoBQH3" ||
+        currentUser === "EVKsigM546MbnakzkDmG0QHlfmn2" ? (
+          <Link href="/claim/new">
             <a>
               <MenuItem>クレーム報告書</MenuItem>
             </a>
           </Link>
         ) : (
-          ''
+          ""
         )}
         <MenuItem onClick={logout}>ログアウト</MenuItem>
       </MenuList>
