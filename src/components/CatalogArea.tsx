24a4f99b6a7d44031f407e8a0c766cb6c621d11e
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-07-22 09:52:29 +0900

    蘇機器カレンダーリンク

--- src/components/CatalogArea.tsx
+++ src/components/CatalogArea.tsx
@@ -1,39 +1,39 @@
-import { Box, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
 
 const CatalogArea = () => {
   return (
     <>
       <Flex
-        width='100%'
-        boxShadow='xs'
-        mt='6'
-        p='3'
-        rounded='md'
-        bg='white'
-        justifyContent={{ base: 'center' }}
-        flexDirection={{ base: 'column', md: 'row', lg: 'column', xl: 'row' }}
+        width="100%"
+        boxShadow="xs"
+        mt="6"
+        p="3"
+        rounded="md"
+        bg="white"
+        justifyContent={{ base: "center" }}
+        flexDirection={{ base: "column", md: "row", lg: "column", xl: "row" }}
       >
-        <Flex flex='2'>
-          <Flex flex='1' p='3' justifyContent='center'>
-            <Link href='https://my.ebook5.net/daimaru-hakui/my-uniform-club/'>
-              <a target='_blank'>
+        <Flex flex="2">
+          <Flex flex="1" p="3" justifyContent="center">
+            <Link href="https://my.ebook5.net/daimaru-hakui/my-uniform-club/">
+              <a target="_blank">
                 <Image
-                  src='/myuni.jpg'
-                  alt='myuni カタログ'
+                  src="/myuni.jpg"
+                  alt="myuni カタログ"
                   width={220}
                   height={300}
                 />
               </a>
             </Link>
           </Flex>
-          <Flex flex='1' p='3' justifyContent='center'>
-            <Link href='https://my.ebook5.net/daimaru-hakui/serenade-vol5/'>
-              <a target='_blank'>
+          <Flex flex="1" p="3" justifyContent="center">
+            <Link href="https://my.ebook5.net/daimaru-hakui/serenade-vol5/">
+              <a target="_blank">
                 <Image
-                  src='/serenade.jpg'
-                  alt='serenade カタログ'
+                  src="/serenade.jpg"
+                  alt="serenade カタログ"
                   width={220}
                   height={300}
                 />
@@ -42,76 +42,75 @@ const CatalogArea = () => {
           </Flex>
         </Flex>
         <Flex
-          flex='1'
-          color='white'
-          flexDirection='column'
-          justifyContent='space-between'
+          flex="1"
+          color="white"
+          flexDirection="column"
+          justifyContent="space-between"
           p={3}
-          w='100%'
-          h='300px'
+          w="100%"
         >
-          <Box h='30%'>
-            <Link href='https://myuni.vercel.app/'>
-              <a target={'_blank'}>
+          <Box h="30%">
+            <Link href="https://myuni.vercel.app/">
+              <a target={"_blank"}>
                 <Flex
-                  h='100%'
-                  minH='50px'
-                  backgroundColor='rgb(45, 55, 72)'
-                  justifyContent='center'
-                  alignItems='center'
+                  h="100%"
+                  minH="50px"
+                  backgroundColor="rgb(45, 55, 72)"
+                  justifyContent="center"
+                  alignItems="center"
                 >
-                  <Box fontSize={{ base: 'xs', md: 'md' }}>
+                  <Box fontSize={{ base: "xs", md: "md" }}>
                     マイユニポータル
                   </Box>
                 </Flex>
               </a>
             </Link>
           </Box>
-          <Box h='30%'>
-            <Link href='https://www.daimaru-hakui.co.jp/'>
-              <a target={'_blank'}>
+          <Box h="30%">
+            <Link href="https://www.daimaru-hakui.co.jp/">
+              <a target={"_blank"}>
                 <Flex
-                  height='100%'
-                  minH='50px'
-                  backgroundColor='rgb(49, 74, 151)'
-                  justifyContent='center'
-                  alignItems='center'
+                  height="100%"
+                  minH="50px"
+                  backgroundColor="rgb(49, 74, 151)"
+                  justifyContent="center"
+                  alignItems="center"
                 >
-                  <Box fontSize={{ base: 'xs', md: 'md' }}>ホームページ</Box>
+                  <Box fontSize={{ base: "xs", md: "md" }}>ホームページ</Box>
                 </Flex>
               </a>
             </Link>
           </Box>
-          <Box h='30%'>
-            <Link href='https://stock-next.vercel.app/'>
-              <a target={'_blank'}>
+          <Box h="30%">
+            <Link href="https://stock-next.vercel.app/">
+              <a target={"_blank"}>
                 <Flex
-                  height='100%'
-                  minH='50px'
-                  backgroundColor='rgb(49, 151, 149)'
-                  justifyContent='center'
-                  alignItems='center'
+                  height="100%"
+                  minH="50px"
+                  backgroundColor="rgb(49, 151, 149)"
+                  justifyContent="center"
+                  alignItems="center"
                 >
-                  <Box fontSize={{ base: 'xs', md: 'md' }}>在庫照会</Box>
+                  <Box fontSize={{ base: "xs", md: "md" }}>在庫照会</Box>
                 </Flex>
               </a>
             </Link>
           </Box>
-          {/* <Box h='30%'>
-            <Link href='https://stock-next.vercel.app/'>
-              <a target={'_blank'}>
+          <Box h="30%">
+            <Link href="https://calendar.nextset.jp/daimaruhakui/gp/calendar.html">
+              <a target={"_blank"}>
                 <Flex
-                  height='100%'
-                  minH='50px'
-                  backgroundColor='#00bcd4'
-                  justifyContent='center'
-                  alignItems='center'
+                  height="100%"
+                  minH="50px"
+                  backgroundColor="#00bcd4"
+                  justifyContent="center"
+                  alignItems="center"
                 >
-                  <Box fontSize={{ base: 'xs', md: 'md' }}>予定表</Box>
+                  <Box fontSize={{ base: "xs", md: "md" }}>組織カレンダー</Box>
                 </Flex>
               </a>
             </Link>
-          </Box> */}
+          </Box>
         </Flex>
       </Flex>
     </>
--- src/pages/claims/[id].tsx
+++ src/pages/claims/[id].tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 //クレーム報告書　個別ページ
-import React, { useEffect, useState } from 'react';
-import { Box, Button, Flex, Input } from '@chakra-ui/react';
+import React, { useEffect, useState } from "react";
+import { Box, Button, Flex, Input } from "@chakra-ui/react";
 import {
   collection,
   deleteDoc,
@@ -11,26 +11,26 @@ import {
   orderBy,
   query,
   updateDoc,
-} from 'firebase/firestore';
-import { deleteObject, ref } from 'firebase/storage';
-import { auth, db, storage } from '../../../firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useRouter } from 'next/router';
-import { useRecoilValue } from 'recoil';
-import { authState } from '../../../store/authState';
-import { taskflow } from '../../../data';
-import { todayDate } from '../../../functions';
+} from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import { auth, db, storage } from "../../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+import { authState } from "../../../store/authState";
+import { taskflow } from "../../../data";
+import { todayDate } from "../../../functions";
 
-import ClaimSelectSendButton from '../../components/claims/button/ClaimSelectSendButton';
-import ClaimReport from '../../components/claims/ClaimReport';
-import ClaimConfirmSendButton from '../../components/claims/button/ClaimConfirmSendButton';
-import ClaimEditButton from '../../components/claims/button/ClaimEditButton';
-import ClaimProgress from '../../components/claims/ClaimProgress';
-import ClaimMessage from '../../components/claims/ClaimMessage';
-import ClaimEditReport from '../../components/claims/ClaimEditReport';
-import Link from 'next/link';
-import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
-import ClaimStampArea from '../../components/claims/ClaimStampArea';
+import ClaimSelectSendButton from "../../components/claims/button/ClaimSelectSendButton";
+import ClaimReport from "../../components/claims/ClaimReport";
+import ClaimConfirmSendButton from "../../components/claims/button/ClaimConfirmSendButton";
+import ClaimEditButton from "../../components/claims/button/ClaimEditButton";
+import ClaimProgress from "../../components/claims/ClaimProgress";
+import ClaimMessage from "../../components/claims/ClaimMessage";
+import ClaimEditReport from "../../components/claims/ClaimEditReport";
+import Link from "next/link";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import ClaimStampArea from "../../components/claims/ClaimStampArea";
 
 //クレーム報告書作成
 
@@ -42,7 +42,7 @@ const ClaimId = () => {
   const [claim, setClaim] = useState<any>([]); //クレームの個別記事を取得
   const [claims, setClaims] = useState<any>([]); //クレーム一覧を取得
   const [users, setUsers] = useState<any>([]); //ユーザー一覧
-  const [selectUser, setSelectUser] = useState(''); //送信先選択
+  const [selectUser, setSelectUser] = useState(""); //送信先選択
   const [selectTask, setSelectTask] = useState<number>(0); //タスクの選択
   const [edit, setEdit] = useState(false); //編集画面切替
   const [isoOfficeUsers, setIsoOfficeUsers] = useState<any>([]);
@@ -50,42 +50,42 @@ const ClaimId = () => {
   const [isoBossUsers, setIsoBossUsers] = useState<any>([]);
   const [isoTopManegmentUsers, setIsoTopManegmentUsers] = useState<any>([]);
 
-  const [customer, setCustomer] = useState(''); //顧客名
-  const [occurrenceDate, setOccurrenceDate] = useState(''); //発生日
-  const [occurrenceSelect, setOccurrenceSelect] = useState(''); //発生選択
-  const [occurrenceContent, setOccurrenceContent] = useState(''); //発生内容
-  const [amendmentSelect, setAmendmentSelect] = useState(''); //修正処置選択
-  const [amendmentContent, setAmendmentContent] = useState(''); //修正処置内容
-  const [counterplanSelect, setCounterplanSelect] = useState(''); //対策選択
-  const [counterplanContent, setCounterplanContent] = useState(''); //対策内容
-  const [completionDate, setCompletionDate] = useState(''); //完了日
-  const [causeDepartmentSelect, setCauseDepartmentSelect] = useState(''); //起因部署
+  const [customer, setCustomer] = useState(""); //顧客名
+  const [occurrenceDate, setOccurrenceDate] = useState(""); //発生日
+  const [occurrenceSelect, setOccurrenceSelect] = useState(""); //発生選択
+  const [occurrenceContent, setOccurrenceContent] = useState(""); //発生内容
+  const [amendmentSelect, setAmendmentSelect] = useState(""); //修正処置選択
+  const [amendmentContent, setAmendmentContent] = useState(""); //修正処置内容
+  const [counterplanSelect, setCounterplanSelect] = useState(""); //対策選択
+  const [counterplanContent, setCounterplanContent] = useState(""); //対策内容
+  const [completionDate, setCompletionDate] = useState(""); //完了日
+  const [causeDepartmentSelect, setCauseDepartmentSelect] = useState(""); //起因部署
 
   const [receptionDate, setReceptionDate] = useState(todayDate); //受付日
-  const [receptionist, setReceptionist] = useState(''); //受付者
-  const [receptionNum, setReceptionNum] = useState(''); //受付NO.
-  const [stampStaff, setStampStaff] = useState(''); //担当者ハンコ
-  const [stampOffice, setStampOffice] = useState(''); //事務局ハンコ
-  const [stampBoss, setStampBoss] = useState(''); //上司ハンコ
-  const [stampManager, setStampManager] = useState(''); //管理者ハンコ
-  const [stampTm, setStampTm] = useState(''); //TMハンコ
-  const [status, setStatus] = useState(''); //ステータス
+  const [receptionist, setReceptionist] = useState(""); //受付者
+  const [receptionNum, setReceptionNum] = useState(""); //受付NO.
+  const [stampStaff, setStampStaff] = useState(""); //担当者ハンコ
+  const [stampOffice, setStampOffice] = useState(""); //事務局ハンコ
+  const [stampBoss, setStampBoss] = useState(""); //上司ハンコ
+  const [stampManager, setStampManager] = useState(""); //管理者ハンコ
+  const [stampTm, setStampTm] = useState(""); //TMハンコ
+  const [status, setStatus] = useState(""); //ステータス
   const [deletedAt, setDeletedAt] = useState(null); //論理削除
   const [createdAt, setCreatedAt] = useState(null); //作成日
 
   const [fileUpload1, setFileUpload1] = useState<any>();
   const [fileUpload2, setFileUpload2] = useState<any>();
   const [fileUpload3, setFileUpload3] = useState<any>();
-  const [imageUrl1, setImageUrl1] = useState('');
-  const [imageUrl2, setImageUrl2] = useState('');
-  const [imageUrl3, setImageUrl3] = useState('');
-  const [imagePath1, setImagePath1] = useState('');
-  const [imagePath2, setImagePath2] = useState('');
-  const [imagePath3, setImagePath3] = useState('');
+  const [imageUrl1, setImageUrl1] = useState("");
+  const [imageUrl2, setImageUrl2] = useState("");
+  const [imageUrl3, setImageUrl3] = useState("");
+  const [imagePath1, setImagePath1] = useState("");
+  const [imagePath2, setImagePath2] = useState("");
+  const [imagePath3, setImagePath3] = useState("");
 
   useEffect(() => {
     if (user === null) {
-      router.push('/login');
+      router.push("/login");
     }
   }, [router, user]);
 
@@ -96,10 +96,10 @@ const ClaimId = () => {
     imagePath2: string,
     imagePath3: string
   ) => {
-    const result = window.confirm('削除して宜しいでしょうか？');
+    const result = window.confirm("削除して宜しいでしょうか？");
     if (!result) return;
 
-    await deleteDoc(doc(db, 'claimList', id));
+    await deleteDoc(doc(db, "claimList", id));
 
     fileDelete(imagePath1);
     fileDelete(imagePath2);
@@ -110,14 +110,14 @@ const ClaimId = () => {
 
   //画像を削除
   const fileDelete = async (path: string) => {
-    if (path === '') {
+    if (path === "") {
       return;
     }
     const imageRef = ref(storage, `${path}`);
     await deleteObject(imageRef)
       .then(() => {
         console.log(path);
-        console.log('削除成功');
+        console.log("削除成功");
       })
       .catch((error) => {
         console.log(error);
@@ -126,7 +126,7 @@ const ClaimId = () => {
 
   //クレーム報告書を受付、担当者に修正処置を依頼
   const acceptClaim = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       status: 1,
       receptionist: currentUser,
@@ -139,19 +139,19 @@ const ClaimId = () => {
 
   //クレーム報告書のステータスを変更
   const switchStatus = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       status: Number(selectTask),
       operator: selectUser,
-      message: '',
+      message: "",
     });
-    router.push('/claims');
+    router.push("/claims");
   };
 
   //クレーム一覧を取得
   useEffect(() => {
-    const claimsCollectionRef = collection(db, 'claimList');
-    const q = query(claimsCollectionRef, orderBy('receptionNum', 'desc'));
+    const claimsCollectionRef = collection(db, "claimList");
+    const q = query(claimsCollectionRef, orderBy("receptionNum", "desc"));
     getDocs(q).then((querySnapshot) => {
       setClaims(
         querySnapshot.docs.map((doc) => ({
@@ -162,16 +162,16 @@ const ClaimId = () => {
     });
   }, []);
 
-  //nextページ prevページを取得
+  //nextページ prevページのIDを取得
   const nextPrevPage = (id: any, page: number) => {
     let currentIndex = 0;
-    claims.forEach((c: any, index: number) => {
-      if (c.id == queryId) {
+    claims.forEach((claim: any, index: number) => {
+      if (claim.id == id) {
         currentIndex = index;
       }
     });
-    const array = claims.filter((c: any, index: number) => {
-      if (currentIndex + page === index) return c.id;
+    const array = claims.filter((claim: any, index: number) => {
+      if (currentIndex + page === index) return claim.id;
     });
     let nextId;
     if (array && array[0]) {
@@ -182,7 +182,7 @@ const ClaimId = () => {
 
   //クレーム報告書を更新
   const updateClaim = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       customer,
       receptionNum,
@@ -201,7 +201,7 @@ const ClaimId = () => {
 
   //クレーム報告書の発生内容を更新
   const updateOccurrenceClaim = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       occurrenceSelect,
       occurrenceContent,
@@ -210,7 +210,7 @@ const ClaimId = () => {
 
   //クレーム報告書の修正処置を更新
   const updateAmendmentClaim = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       amendmentSelect,
       amendmentContent,
@@ -220,7 +220,7 @@ const ClaimId = () => {
 
   //クレーム報告書を担当者入力欄の更新
   const updateStaffClaim = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       occurrenceSelect,
       occurrenceContent,
@@ -231,7 +231,7 @@ const ClaimId = () => {
 
   //クレーム報告書を対策者・上司入力欄の更新
   const updateCounterplanClaim = async (id: any) => {
-    const docRef = doc(db, 'claimList', id);
+    const docRef = doc(db, "claimList", id);
     await updateDoc(docRef, {
       counterplanSelect,
       counterplanContent,
@@ -241,15 +241,15 @@ const ClaimId = () => {
 
   // クレーム報告書を取得;
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, 'claimList', `${queryId}`), (doc) => {
+    const unsub = onSnapshot(doc(db, "claimList", `${queryId}`), (doc) => {
       setClaim(doc.data());
     });
   }, [queryId, edit]);
 
   //ユーザー一覧を取得
   useEffect(() => {
-    const usersCollectionRef = collection(db, 'authority');
-    const q = query(usersCollectionRef, orderBy('rank', 'asc'));
+    const usersCollectionRef = collection(db, "authority");
+    const q = query(usersCollectionRef, orderBy("rank", "asc"));
     const unsub = onSnapshot(q, (querySnapshot: any) => {
       setUsers(
         querySnapshot.docs.map((doc: any) => ({
@@ -399,35 +399,35 @@ const ClaimId = () => {
 
   //編集をキャンセルしたときに、setを空にする
   const editCancel = () => {
-    setCustomer('');
-    setOccurrenceDate('');
-    setOccurrenceSelect('');
-    setOccurrenceContent('');
-    setAmendmentSelect('');
-    setAmendmentContent('');
-    setCounterplanSelect('');
-    setCounterplanContent('');
-    setReceptionNum('');
-    setReceptionDate('');
-    setCompletionDate('');
-    setImageUrl1('');
-    setImageUrl2('');
-    setImageUrl3('');
-    setImagePath1('');
-    setImagePath2('');
-    setImagePath3('');
+    setCustomer("");
+    setOccurrenceDate("");
+    setOccurrenceSelect("");
+    setOccurrenceContent("");
+    setAmendmentSelect("");
+    setAmendmentContent("");
+    setCounterplanSelect("");
+    setCounterplanContent("");
+    setReceptionNum("");
+    setReceptionDate("");
+    setCompletionDate("");
+    setImageUrl1("");
+    setImageUrl2("");
+    setImageUrl3("");
+    setImagePath1("");
+    setImagePath2("");
+    setImagePath3("");
   };
 
   return (
     <>
       {claim && currentUser && (
         <>
-          <Box w='100%' p={6} backgroundColor={'#f7f7f7'} position='relative'>
-            <Flex justifyContent='space-between' color='gray.600'>
+          <Box w="100%" p={6} backgroundColor={"#f7f7f7"} position="relative">
+            <Flex justifyContent="space-between" color="gray.600">
               {nextPrevPage(queryId, 1) !== undefined ? (
                 <Link href={`/claims/${nextPrevPage(queryId, 1)}`}>
                   <a>
-                    <Flex alignItems='center'>
+                    <Flex alignItems="center">
                       <ArrowBackIcon />
                       前のクレーム
                     </Flex>
@@ -440,7 +440,7 @@ const ClaimId = () => {
               {nextPrevPage(queryId, -1) !== undefined ? (
                 <Link href={`/claims/${nextPrevPage(queryId, -1)}`}>
                   <a>
-                    <Flex alignItems='center'>
+                    <Flex alignItems="center">
                       次のクレーム
                       <ArrowForwardIcon />
                     </Flex>
@@ -481,10 +481,10 @@ const ClaimId = () => {
 
             {/* レポート部分メイン */}
             <Box
-              w={{ base: '100%', md: '750px' }}
-              mx='auto'
+              w={{ base: "100%", md: "750px" }}
+              mx="auto"
               p={6}
-              backgroundColor='white'
+              backgroundColor="white"
               borderRadius={6}
             >
               {/* 受付ナンバー　受付日 */}
@@ -492,18 +492,18 @@ const ClaimId = () => {
                 <>
                   {!edit && (
                     <Flex
-                      alignItems='center'
-                      justifyContent='space-between'
-                      w='100%'
+                      alignItems="center"
+                      justifyContent="space-between"
+                      w="100%"
                     >
-                      <Flex mr={1} alignItems='center'>
-                        <Box fontSize='lg' fontWeight='semibold' mr={1}>
+                      <Flex mr={1} alignItems="center">
+                        <Box fontSize="lg" fontWeight="semibold" mr={1}>
                           受付NO
                         </Box>
                         <Box>{claim.receptionNum}</Box>
                       </Flex>
-                      <Flex alignItems='center'>
-                        <Box fontSize='lg' fontWeight='semibold' mr={1}>
+                      <Flex alignItems="center">
+                        <Box fontSize="lg" fontWeight="semibold" mr={1}>
                           受付日
                         </Box>
                         <Box>{claim.receptionDate}</Box>
@@ -577,34 +577,34 @@ const ClaimId = () => {
               {/*'未処理 受付NO. 受付日 入力欄*/}
               {Number(claim.status) === 0 && enabledOffice() && (
                 <>
-                  <Flex alignItems='center' w='100%' mt={10}>
-                    <Flex mr={5} alignItems='center'>
-                      <Box fontSize='lg' fontWeight='semibold' minW='70px'>
+                  <Flex alignItems="center" w="100%" mt={10}>
+                    <Flex mr={5} alignItems="center">
+                      <Box fontSize="lg" fontWeight="semibold" minW="70px">
                         受付NO
                       </Box>
                       <Input
-                        type='text'
-                        placeholder='例 4-001'
+                        type="text"
+                        placeholder="例 4-001"
                         value={receptionNum}
                         onChange={(e) => setReceptionNum(e.target.value)}
                       />
                     </Flex>
-                    <Flex mr={1} alignItems='center'>
-                      <Box fontSize='lg' fontWeight='semibold' minW='70px'>
+                    <Flex mr={1} alignItems="center">
+                      <Box fontSize="lg" fontWeight="semibold" minW="70px">
                         受付日
                       </Box>
                       <Input
-                        type='date'
+                        type="date"
                         value={receptionDate}
                         onChange={(e) => setReceptionDate(e.target.value)}
                       />
                     </Flex>
                   </Flex>
-                  <Flex justifyContent='center'>
+                  <Flex justifyContent="center">
                     <Button
                       mt={6}
                       mr={3}
-                      colorScheme='blue'
+                      colorScheme="blue"
                       onClick={() => {
                         acceptClaim(queryId);
                       }}
@@ -614,7 +614,7 @@ const ClaimId = () => {
                     </Button>
                     <Button
                       mt={6}
-                      colorScheme='red'
+                      colorScheme="red"
                       onClick={() =>
                         deleteClaim(
                           queryId,
