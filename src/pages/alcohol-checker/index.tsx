5450b8ad27c87e680d07ab934c2298d75eff16a0
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-08-23 16:43:28 +0900

    アルコール一覧

--- src/pages/alcohol-checker/index.tsx
+++ src/pages/alcohol-checker/index.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Flex,
   Table,
   TableContainer,
@@ -8,17 +9,18 @@ import {
   Th,
   Thead,
   Tr,
-} from '@chakra-ui/react';
-import { collection, doc, getDocs, onSnapshot } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { db } from '../../../firebase';
+} from "@chakra-ui/react";
+import { collection, getDocs } from "firebase/firestore";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { db } from "../../../firebase";
 
 const Alcohol = () => {
-  const [items, setItems] = useState<any>([]);
+  const [posts, setPosts] = useState<any>([]);
   useEffect(() => {
-    const collectionRef = collection(db, 'alcoholCheckList');
+    const collectionRef = collection(db, "alcoholCheckList");
     getDocs(collectionRef).then((querySnapshot) => {
-      setItems(
+      setPosts(
         querySnapshot.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
@@ -26,29 +28,44 @@ const Alcohol = () => {
       );
     });
   }, []);
-  console.log(items);
+  console.log(posts);
   return (
     <Box
       p={6}
-      backgroundColor={'#f7f7f7'}
-      paddingBottom={'50px'}
-      minH={'100vh'}
+      backgroundColor={"#f7f7f7"}
+      paddingBottom={"50px"}
+      minH={"100vh"}
     >
-      <Flex flexDirection={'column'} alignItems={'center'}>
-        <TableContainer backgroundColor='white' borderRadius={6} p={6}>
-          <Flex mb={6} justifyContent='space-between'>
-            <Box fontSize='lg'>全10件</Box>
+      <Flex flexDirection={"column"} alignItems={"center"}>
+        <TableContainer backgroundColor="white" borderRadius={6} p={6}>
+          <Flex mb={6} justifyContent="space-between">
+            <Box fontSize="lg">全{posts.length}件</Box>
           </Flex>
-          <Table size='sm'>
+          <Table size="sm">
             <Thead>
               <Tr>
-                <Th minW='80px'>日付</Th>
-                <Th minW='95px'>ステータス</Th>
-                <Th minW='105px'>受付日</Th>
-                <Th minW='80px'>受付NO.</Th>
+                <Th minW="130x">日付</Th>
+                <Th minW="50px">完了</Th>
+                <Th minW="50px">未処理</Th>
+                <Th></Th>
               </Tr>
             </Thead>
-            <Tbody></Tbody>
+            <Tbody>
+              {posts.map((post: { id: string; member: string[] }) => (
+                <Tr key={post.id}>
+                  <Td>{post.id}</Td>
+                  <Td>{post.member.length}名</Td>
+                  <Td>{24 - post.member.length}名</Td>
+                  <Td>
+                    <Link href={`alcohol-checker/${post.id}`}>
+                      <a>
+                        <Button>詳細</Button>
+                      </a>
+                    </Link>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
           </Table>
         </TableContainer>
       </Flex>
