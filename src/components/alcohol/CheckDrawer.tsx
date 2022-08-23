6fd390cd2a7d7814062e2f62bdab352e2b899d2e
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-08-23 10:42:37 +0900

    alcohol data

---
+++ src/components/alcohol/CheckDrawer.tsx
@@ -0,0 +1,177 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import React, { memo, useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { db } from "../../../firebase";
+import { todayDate } from "../../../functions";
+import { authState } from "../../../store";
+
+const CheckDrawer = () => {
+  const currentUser = useRecoilValue(authState);
+  const [alcoholList, setAlcoholList] = useState<any>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  //アルコールチェック登録
+  const setAlcoholCheckList = async () => {
+    try {
+      const docListRef = doc(db, "alcoholCheckList", `${todayDate()}`);
+      const docSnap = await getDoc(docListRef);
+      if (docSnap.exists()) {
+        await updateDoc(docListRef, {
+          member: arrayUnion(currentUser),
+        });
+      } else {
+        await setDoc(docListRef, {
+          id: `${todayDate()}`,
+          member: arrayUnion(currentUser),
+        });
+      }
+      const docDataRef = await addDoc(collection(db, "alcoholCheckData"), {
+        date: `${todayDate()}`,
+        user: currentUser,
+        createdAt: serverTimestamp(),
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    const docRef = doc(db, "alcoholCheckList", `${todayDate()}`);
+    const unsub = onSnapshot(docRef, (querySnapshot) => {
+      setAlcoholList(querySnapshot.data());
+    });
+  }, []);
+
+  // const setAlcoholCheckList = async () => {
+  //   try {
+  //     const docRef = doc(db, 'alcoholCheckList', `${todayDate()}`);
+  //     const docSnap = await getDoc(docRef);
+  //     if (!docSnap.exists()) {
+  //       await setDoc(docRef, {
+  //         [currentUser]: {
+  //           uid: currentUser,
+  //           updatedAt: serverTimestamp(),
+  //         },
+  //       });
+  //     } else {
+  //       await updateDoc(docRef, {
+  //         [currentUser]: {
+  //           uid: currentUser,
+  //           updatedAt: serverTimestamp(),
+  //         },
+  //       });
+  //     }
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+
+  return (
+    <>
+      {alcoholList &&
+      alcoholList.member &&
+      alcoholList.member.includes(currentUser) ? (
+        ""
+      ) : (
+        <>
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            mt={6}
+            p={6}
+            width={{ base: "100%" }}
+            rounded="md"
+            backgroundColor="white"
+            boxShadow="xs"
+          >
+            <Box textAlign="center" mr={6}>
+              アルコールチェックをしてください
+            </Box>
+            <Button colorScheme="blue" onClick={onOpen}>
+              Click
+            </Button>
+          </Flex>
+        </>
+      )}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>アルコールチェック</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <>
+              <Box>
+                <Box>アルコールの検査はしましたか？</Box>
+                <RadioGroup defaultValue="2">
+                  <Stack spacing={8} direction="row" mt={1}>
+                    <Radio colorScheme="red" value="1">
+                      No
+                    </Radio>
+                    <Radio colorScheme="green" value="2">
+                      Yes
+                    </Radio>
+                  </Stack>
+                </RadioGroup>
+              </Box>
+              <Box mt={3}>
+                <Box>酒気帯び</Box>
+                <RadioGroup defaultValue="2" mt={1}>
+                  <Stack spacing={9} direction="row">
+                    <Radio colorScheme="red" value="1">
+                      有
+                    </Radio>
+                    <Radio colorScheme="green" value="2">
+                      無
+                    </Radio>
+                  </Stack>
+                </RadioGroup>
+              </Box>
+            </>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              width="100px"
+              colorScheme="facebook"
+              onClick={() => {
+                setAlcoholCheckList();
+                onClose();
+              }}
+            >
+              提出
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CheckDrawer;
