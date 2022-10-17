3f08b21bc0a515eed69377f1bde840159ea558a1
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-10-17 15:31:16 +0900

    sales 一覧完成

--- src/pages/sales/index.tsx
+++ src/pages/sales/index.tsx
@@ -1,4 +1,17 @@
-import { Box, Container, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import {
   collection,
   endAt,
@@ -7,24 +20,37 @@ import {
   query,
   startAt,
   where,
-} from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { db } from '../../../firebase';
-import { usersState } from '../../../store';
+} from "firebase/firestore";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Administrator } from "../../../data";
+import { db } from "../../../firebase";
+import { authState, usersState } from "../../../store";
+import SalesEditModal from "../../components/sales/SalesEditModal";
 
 const Sales = () => {
+  const currentUser = useRecoilValue(authState);
   const [users, setUsers] = useRecoilState<any>(usersState); //ユーザー一覧リスト
   const [sales, setSeles] = useState<any>();
-  const [sum, setSum] = useState<number>();
-  const [recentAtDate, setRecentAtDate] = useState('2022-10-1');
-  const [sinceAtDate, setSinceAtDate] = useState('2022-10-31');
+  const [targetSum, setTargetSum] = useState<number>();
+  const [landingSum, setLandingSum] = useState<number>();
+  // const [recentAtDate, setRecentAtDate] = useState(new Date());
+  // const [sinceAtDate, setSinceAtDate] = useState("2022-10-31");
 
+  // salesデータを取得
   useEffect(() => {
-    const salesCollectionRef = collection(db, 'sales');
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const lastDate = new Date(year, month, 0);
+    const recentAtDate = `${year}-${month}-1`;
+    const sinceAtDate = `${year}-${month}-${lastDate.getDate()}`;
+
+    const salesCollectionRef = collection(db, "sales");
     const q = query(
       salesCollectionRef,
-      orderBy('datetime'),
+      orderBy("datetime"),
       startAt(recentAtDate),
       endAt(sinceAtDate)
     );
@@ -37,8 +63,9 @@ const Sales = () => {
       );
     });
     return unsub;
-  }, [recentAtDate, sinceAtDate]);
+  }, []);
 
+  // 名前表示
   const dispName = (uid: string) => {
     const userName = users.find((user: any) => {
       if (user.uid === uid) return true;
@@ -46,29 +73,87 @@ const Sales = () => {
     return userName?.name;
   };
 
+  // 目標・着地　合計値
   useEffect(() => {
-    let sum = 0;
+    let target = 0;
     sales?.forEach((sale: any) => {
-      sum += Number(sale.currentSales);
+      target += Number(sale.currentTarget);
     });
-    setSum(sum);
+    setTargetSum(target);
+
+    let landing = 0;
+    sales?.forEach((sale: any) => {
+      landing += Number(sale.currentLanding);
+    });
+    setLandingSum(landing);
   }, [sales]);
 
   return (
-    <Container>
-      <Box my={6}>
-        {sales?.map((sale: any) => (
-          <Flex key={sale.id}>
-            <Box mr={6}>{dispName(sale.currentUser)}</Box>
-            <Box>{sale.currentSales}</Box>
-          </Flex>
-        ))}
-        <Flex mt={6}>
-          <Box>合計</Box>
-          <Box>{sum}</Box>
-        </Flex>
-      </Box>
-    </Container>
+    <Box backgroundColor={"#f7f7f7"} pt={6} minH={"calc(100vh - 135px)"}>
+      <Container maxW="700px" mt={6}>
+        <Box my={3} fontSize="xl">
+          今月売上一覧
+        </Box>
+        <TableContainer bgColor="white" borderRadius={6} p={6}>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>担当</Th>
+                <Th>目標売上</Th>
+                <Th>着地売上</Th>
+                <Th>差額</Th>
+                <Th>編集</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {sales?.map((sale: any) => (
+                <Tr key={sale.id}>
+                  <Td mr={6}>{dispName(sale.currentUser)}</Td>
+                  <Td isNumeric>
+                    {Number(sale.currentTarget).toLocaleString()}
+                  </Td>
+                  <Td isNumeric>
+                    {Number(sale.currentLanding).toLocaleString()}
+                  </Td>
+                  <Td isNumeric>
+                    {(
+                      Number(sale.currentLanding) - Number(sale.currentTarget)
+                    ).toLocaleString()}
+                  </Td>
+                  <Td>
+                    {(Administrator.includes(currentUser) ||
+                      sale.currentUser === currentUser) && (
+                      <SalesEditModal docId={"2022-10_" + sale.currentUser} />
+                    )}
+                  </Td>
+                </Tr>
+              ))}
+              <Tr>
+                <Td mr={6} fontWeight="bold">
+                  合計
+                </Td>
+                <Td fontWeight="bold" isNumeric>
+                  {targetSum?.toLocaleString()}
+                </Td>
+                <Td fontWeight="bold" isNumeric>
+                  {landingSum?.toLocaleString()}
+                </Td>
+                <Td fontWeight="bold" isNumeric>
+                  {(Number(landingSum) - Number(targetSum)).toLocaleString()}
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <Box mt={6} textAlign="center">
+          <Link href="/sales/new">
+            <a>
+              <Button colorScheme="blue">売上登録</Button>
+            </a>
+          </Link>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
--- src/pages/sales/new.tsx
+++ src/pages/sales/new.tsx
@@ -1,16 +1,26 @@
-import { Box, Button, Container, Flex, Input, Text } from '@chakra-ui/react';
-import { addDoc, doc, serverTimestamp, setDoc } from 'firebase/firestore';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import { db } from '../../../firebase';
-import { authState } from '../../../store';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { db } from "../../../firebase";
+import { authState } from "../../../store";
 
 const SalesNew = () => {
   const router = useRouter();
   const currentUser = useRecoilValue(authState);
   const [salesObj, setSalesObj] = useState({
-    currentSales: 0,
+    currentTarget: "",
+    currentLanding: "",
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,31 +34,68 @@ const SalesNew = () => {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
-    const result = year + '-' + month;
+    const result = year + "-" + month;
 
-    const docRef = doc(db, 'sales', `${result}_${currentUser}`);
-    await setDoc(docRef, {
-      currentSales: salesObj.currentSales,
-      currentUser,
-      createdAt: serverTimestamp(),
-      datetime: year + '-' + month + '-' + day,
-    });
+    const docRef = doc(db, "sales", `${result}_${currentUser}`);
+    try {
+      await setDoc(docRef, {
+        currentTarget: salesObj.currentTarget,
+        currentLanding: salesObj.currentLanding,
+        currentUser,
+        createdAt: serverTimestamp(),
+        datetime: `${year}-${month}-${day}`,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      router.push("/sales/");
+    }
   };
+
+  const dateTitle = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const result = year + "-" + month;
+    return result;
+  };
+
   return (
-    <Container>
-      <Box my={6}>
-        <Text>売上</Text>
-        <Flex>
-          <Input
-            mr={2}
-            name='currentSales'
-            value={salesObj.currentSales}
-            onChange={handleInputChange}
-          />
-          <Button onClick={addSales}>登録</Button>
-        </Flex>
-      </Box>
-    </Container>
+    <Box backgroundColor={"#f7f7f7"} pt={6} minH={"calc(100vh - 135px)"}>
+      <Container bgColor="white" p={6} borderRadius={6}>
+        <Box>
+          <Stack spacing={6}>
+            <Text>{dateTitle()}月 売上</Text>
+            <Input
+              mr={2}
+              placeholder="目標額を入力してください"
+              name="currentTarget"
+              value={salesObj.currentTarget}
+              onChange={handleInputChange}
+            />
+            <Input
+              mr={2}
+              placeholder="着地金額を入力してください"
+              name="currentLanding"
+              value={salesObj.currentLanding}
+              onChange={handleInputChange}
+            />
+            <Flex w="100%">
+              <Box w="50%" mr={3}>
+                <Link href="/sales/">
+                  <a>
+                    <Button w="100%">戻る</Button>
+                  </a>
+                </Link>
+              </Box>
+              <Button w="50%" colorScheme="blue" onClick={addSales}>
+                登録
+              </Button>
+            </Flex>
+          </Stack>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
