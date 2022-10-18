c14d2ccf00e382825fc938c32f09479e37b43832
daimaru-hakui <dh.mukai.dh@gmail.com>
2022-10-18 16:06:13 +0900

    権限設定　追加

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -11,7 +11,7 @@ import {
   Tr,
   Wrap,
   WrapItem,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {
   collection,
   doc,
@@ -20,15 +20,15 @@ import {
   query,
   setDoc,
   updateDoc,
-} from 'firebase/firestore';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useRecoilValue } from 'recoil';
-import { Users, Administrator } from '../../../data';
-import { auth, db } from '../../../firebase';
-import { authState } from '../../../store';
-import Header from '../../components/Header';
+} from "firebase/firestore";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilValue } from "recoil";
+import { Users, Administrator } from "../../../data";
+import { auth, db } from "../../../firebase";
+import { authState } from "../../../store";
+import AdminEditModal from "../../components/admin/AdminEditModal";
 
 const Admin = () => {
   const [user] = useAuthState(auth);
@@ -40,14 +40,14 @@ const Admin = () => {
   //ログインしていなかったらログイン画面へ
   useEffect(() => {
     if (user === null) {
-      router.push('/login');
+      router.push("/login");
     }
   }, [router, user]);
 
   //初期設定に追加
   const setting = async (user: { uid: string; name: string; rank: number }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await setDoc(docRef, {
         uid: user.uid,
         name: user.name,
@@ -67,7 +67,7 @@ const Admin = () => {
   //トップマネジメントに追加
   const addTopManegment = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoTopManegment: true,
       });
@@ -79,7 +79,7 @@ const Admin = () => {
   //ISO管理者に追加
   const addManager = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoManager: true,
       });
@@ -91,7 +91,7 @@ const Admin = () => {
   //ISO 上司に追加
   const addBoss = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid); //topManegmentGroupはfirestoreで直接登録
+      const docRef = doc(db, "authority", user.uid); //topManegmentGroupはfirestoreで直接登録
       await updateDoc(docRef, {
         isoBoss: true,
       });
@@ -103,7 +103,7 @@ const Admin = () => {
   //ISO 事務局に追加
   const addOffice = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid); //topManegmentGroupはfirestoreで直接登録
+      const docRef = doc(db, "authority", user.uid); //topManegmentGroupはfirestoreで直接登録
       await updateDoc(docRef, {
         isoOffice: true,
       });
@@ -115,7 +115,7 @@ const Admin = () => {
   //営業担当に追加
   const addSalesStaff = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoSalesStaff: true,
       });
@@ -127,7 +127,7 @@ const Admin = () => {
   //アルコールチェッカー管理に追加
   const addAlcoholChecker = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         alcoholChecker: true,
       });
@@ -139,7 +139,7 @@ const Admin = () => {
   //トップマネジメントから削除
   const removeTopManegment = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid); //topManegmentGroupはfirestoreで直接登録
+      const docRef = doc(db, "authority", user.uid); //topManegmentGroupはfirestoreで直接登録
       await updateDoc(docRef, {
         isoTopManegment: false,
       });
@@ -150,7 +150,7 @@ const Admin = () => {
   //管理者から削除
   const removeManager = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoManager: false,
       });
@@ -161,7 +161,7 @@ const Admin = () => {
   //上司から削除
   const removeBoss = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoBoss: false,
       });
@@ -172,7 +172,7 @@ const Admin = () => {
   //事務局から削除
   const removeOffice = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoOffice: false,
       });
@@ -184,7 +184,7 @@ const Admin = () => {
   //営業担当から削除
   const removeSalesStaff = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         isoSalesStaff: false,
       });
@@ -196,7 +196,7 @@ const Admin = () => {
   //アルコールチェッカーから削除
   const removeAlcoholChecker = async (user: { uid: string; name: string }) => {
     try {
-      const docRef = doc(db, 'authority', user.uid);
+      const docRef = doc(db, "authority", user.uid);
       await updateDoc(docRef, {
         alcoholChecker: false,
       });
@@ -207,8 +207,8 @@ const Admin = () => {
 
   //firestore authority 情報の取得
   useEffect(() => {
-    const usersCollectionRef = collection(db, 'authority');
-    const q = query(usersCollectionRef, orderBy('rank', 'asc'));
+    const usersCollectionRef = collection(db, "authority");
+    const q = query(usersCollectionRef, orderBy("rank", "asc"));
     const unsub = onSnapshot(q, (querySnapshot) => {
       setUsersRegisterList(
         querySnapshot.docs.map((doc) => ({
@@ -224,38 +224,40 @@ const Admin = () => {
     <>
       {Administrator.includes(currentUser) && (
         <Box
-          w={'100%'}
-          backgroundColor={'#f7f7f7'}
-          paddingBottom={'50px'}
-          minH={'100vh'}
+          w={"100%"}
+          backgroundColor={"#f7f7f7"}
+          paddingBottom={"50px"}
+          minH={"100vh"}
           p={6}
         >
           <Box
             p={6}
-            mx={'auto'}
-            w={{ base: '100%', md: '900px' }}
-            bg='white'
-            borderRadius={'5px'}
+            mx={"auto"}
+            w={{ base: "100%", md: "1200px" }}
+            bg="white"
+            borderRadius={"5px"}
           >
-            <Box as='h1' fontSize='4xl'>
+            <Box as="h1" fontSize="4xl">
               管理者設定画面
             </Box>
             <Box mt={12}>
-              <Text as='h2' fontSize='2xl'>
+              <Text as="h2" fontSize="2xl">
                 権限設定
               </Text>
               <Box p={6}>
                 <TableContainer>
-                  <Table size='sm' width={'100%'}>
+                  <Table size="sm" width={"100%"}>
                     <Thead>
                       <Tr>
+                        <Th>id</Th>
                         <Th>名前</Th>
                         <Th>ISO TM</Th>
                         <Th>ISO 管理者</Th>
                         <Th>ISO 上長</Th>
                         <Th>ISO 事務局</Th>
                         <Th>営業・販売</Th>
-                        <Th>アルコールチェック管理</Th>
+                        <Th>ALチェック管理</Th>
+                        <Th>編集</Th>
                       </Tr>
                     </Thead>
                     <Tbody>
@@ -269,19 +271,25 @@ const Admin = () => {
                           isoOffice: boolean;
                           isoSalesStaff: boolean;
                           alcoholChecker: boolean;
+                          rank: number;
                         }) => (
                           <Tr key={user.uid}>
+                            <Td>{user?.rank}</Td>
                             <Td>{user.name}</Td>
                             <Td>
                               {user.isoTopManegment ? (
                                 <Button
+                                  size="xs"
                                   onClick={() => removeTopManegment(user)}
-                                  colorScheme='blue'
+                                  colorScheme="blue"
                                 >
                                   有効
                                 </Button>
                               ) : (
-                                <Button onClick={() => addTopManegment(user)}>
+                                <Button
+                                  size="xs"
+                                  onClick={() => addTopManegment(user)}
+                                >
                                   無効
                                 </Button>
                               )}
@@ -289,13 +297,17 @@ const Admin = () => {
                             <Td>
                               {user.isoManager ? (
                                 <Button
+                                  size="xs"
                                   onClick={() => removeManager(user)}
-                                  colorScheme='blue'
+                                  colorScheme="blue"
                                 >
                                   有効
                                 </Button>
                               ) : (
-                                <Button onClick={() => addManager(user)}>
+                                <Button
+                                  size="xs"
+                                  onClick={() => addManager(user)}
+                                >
                                   無効
                                 </Button>
                               )}
@@ -303,13 +315,14 @@ const Admin = () => {
                             <Td>
                               {user.isoBoss ? (
                                 <Button
+                                  size="xs"
                                   onClick={() => removeBoss(user)}
-                                  colorScheme='blue'
+                                  colorScheme="blue"
                                 >
                                   有効
                                 </Button>
                               ) : (
-                                <Button onClick={() => addBoss(user)}>
+                                <Button size="xs" onClick={() => addBoss(user)}>
                                   無効
                                 </Button>
                               )}
@@ -317,13 +330,17 @@ const Admin = () => {
                             <Td>
                               {user.isoOffice ? (
                                 <Button
+                                  size="xs"
                                   onClick={() => removeOffice(user)}
-                                  colorScheme='blue'
+                                  colorScheme="blue"
                                 >
                                   有効
                                 </Button>
                               ) : (
-                                <Button onClick={() => addOffice(user)}>
+                                <Button
+                                  size="xs"
+                                  onClick={() => addOffice(user)}
+                                >
                                   無効
                                 </Button>
                               )}
@@ -331,13 +348,17 @@ const Admin = () => {
                             <Td>
                               {user.isoSalesStaff ? (
                                 <Button
+                                  size="xs"
                                   onClick={() => removeSalesStaff(user)}
-                                  colorScheme='blue'
+                                  colorScheme="blue"
                                 >
                                   有効
                                 </Button>
                               ) : (
-                                <Button onClick={() => addSalesStaff(user)}>
+                                <Button
+                                  size="xs"
+                                  onClick={() => addSalesStaff(user)}
+                                >
                                   無効
                                 </Button>
                               )}
@@ -345,16 +366,23 @@ const Admin = () => {
                             <Td>
                               {user.alcoholChecker ? (
                                 <Button
+                                  size="xs"
                                   onClick={() => removeAlcoholChecker(user)}
-                                  colorScheme='blue'
+                                  colorScheme="blue"
                                 >
                                   有効
                                 </Button>
                               ) : (
-                                <Button onClick={() => addAlcoholChecker(user)}>
+                                <Button
+                                  size="xs"
+                                  onClick={() => addAlcoholChecker(user)}
+                                >
                                   無効
                                 </Button>
                               )}
+                            </Td>
+                            <Td>
+                              <AdminEditModal uid={user.uid} />
                             </Td>
                           </Tr>
                         )
@@ -362,10 +390,10 @@ const Admin = () => {
                     </Tbody>
                   </Table>
                 </TableContainer>
-                {currentUser === 'MBTOK9Jr0eRWVuoT2YXgZNMoBQH3' && (
+                {/* {currentUser === "MBTOK9Jr0eRWVuoT2YXgZNMoBQH3" && (
                   <Box mt={12}>
                     <Text>初期設定（アプリに登録する）</Text>
-                    <Wrap spacing='5px' p={3}>
+                    <Wrap spacing="5px" p={3}>
                       {Users.map(
                         (user: { uid: string; name: string; rank: number }) => (
                           <WrapItem key={user.uid}>
@@ -377,7 +405,7 @@ const Admin = () => {
                       )}
                     </Wrap>
                   </Box>
-                )}
+                )} */}
               </Box>
             </Box>
           </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,39 @@
-import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import Information from '../components/Information';
-import QuickLink from '../components/QuickLink';
-import Slogan from '../components/Slogan';
-import CatalogArea from '../components/CatalogArea';
-import RecruitmentPosts from '../components/recruitmentComp/RecruitmentPosts';
-import styles from '../styles/Home.module.css';
-import { Box, Flex, Tab, TabList, Tabs, Text } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
-import { auth } from '../../firebase';
-import { db } from '../../firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Information from "../components/Information";
+import QuickLink from "../components/QuickLink";
+import Slogan from "../components/Slogan";
+import CatalogArea from "../components/CatalogArea";
+import RecruitmentPosts from "../components/recruitmentComp/RecruitmentPosts";
+import styles from "../styles/Home.module.css";
+import { Box, Flex, Tab, TabList, Tabs, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { auth } from "../../firebase";
+import { db } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import {
   collection,
   doc,
+  getDoc,
   getDocs,
   onSnapshot,
   orderBy,
   query,
+  setDoc,
   where,
-} from 'firebase/firestore';
-import { useRecoilState, useRecoilValue } from 'recoil';
+} from "firebase/firestore";
+import { useRecoilState, useRecoilValue } from "recoil";
 import {
   authState,
   claimsState,
   hideRequestsState,
   requestsState,
   usersState,
-} from '../../store/';
-import CheckDrawer from '../components/alcohol/CheckDrawer';
-import { datetime, todayDate } from '../../functions';
-import Link from 'next/link';
+} from "../../store/";
+import CheckDrawer from "../components/alcohol/CheckDrawer";
+import { datetime, todayDate } from "../../functions";
+import Link from "next/link";
 
 const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
   const [user] = useAuthState(auth);
@@ -54,17 +56,17 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
 
   useEffect(() => {
     if (user === null) {
-      router.push('/login');
+      router.push("/login");
     }
   }, [router, user]);
 
   //掲載中（表示）案件
   useEffect(() => {
-    const requestsCollectionRef = collection(db, 'requestList');
+    const requestsCollectionRef = collection(db, "requestList");
     const q = query(
       requestsCollectionRef,
-      where('display', '==', true),
-      orderBy('sendAt', 'desc')
+      where("display", "==", true),
+      orderBy("sendAt", "desc")
     );
     const unsub = onSnapshot(q, (querySnapshot) => {
       setRequests(
@@ -79,11 +81,11 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
 
   //終了（非表示）案件
   useEffect(() => {
-    const requestCollectionRef = collection(db, 'requestList');
+    const requestCollectionRef = collection(db, "requestList");
     const q = query(
       requestCollectionRef,
-      where('display', '==', false),
-      orderBy('sendAt', 'desc')
+      where("display", "==", false),
+      orderBy("sendAt", "desc")
     );
     const unsub = onSnapshot(q, (querySnapshot) => {
       setHideRequests(
@@ -98,8 +100,8 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
 
   //users情報
   useEffect(() => {
-    const usersCollectionRef = collection(db, 'authority');
-    const q = query(usersCollectionRef, orderBy('rank', 'asc'));
+    const usersCollectionRef = collection(db, "authority");
+    const q = query(usersCollectionRef, orderBy("rank", "asc"));
     getDocs(q).then((querySnapshot) => {
       setUsers(
         querySnapshot.docs.map((doc) => ({
@@ -109,11 +111,27 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
       );
     });
   }, [setUsers]);
+  console.log(currentUser);
+
+  // 未登録であればauthorityに登録
+  useEffect(() => {
+    const docRef = doc(db, "authority", `${currentUser}`);
+    const addAuthority = async () => {
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) return;
+      await setDoc(docRef, {
+        uid: currentUser,
+        name: user?.email,
+        rank: 1000,
+      });
+    };
+    addAuthority();
+  }, [currentUser, user]);
 
   //【クレーム】クレーム一覧リスト取得
   useEffect(() => {
-    const claimsCollectionRef = collection(db, 'claimList');
-    const q = query(claimsCollectionRef, orderBy('receptionNum', 'desc'));
+    const claimsCollectionRef = collection(db, "claimList");
+    const q = query(claimsCollectionRef, orderBy("receptionNum", "desc"));
     const unsub = onSnapshot(q, (querySnapshot) => {
       setClaims(
         querySnapshot.docs.map((doc) => ({
@@ -182,7 +200,7 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
   //アルコールチェック
   useEffect(() => {
     const unsub = onSnapshot(
-      doc(db, 'alcoholCheckList', `${todayDate()}`),
+      doc(db, "alcoholCheckList", `${todayDate()}`),
       (doc) => {
         setAlcoholObject(doc.data());
       }
@@ -206,46 +224,46 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
     <>
       <Head>
         <title>大丸白衣ポータル</title>
-        <meta name='description' content='大丸白衣ポータル' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="大丸白衣ポータル" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       {currentUser && (
-        <div style={{ backgroundColor: '#f7f7f7' }}>
+        <div style={{ backgroundColor: "#f7f7f7" }}>
           <div className={styles.container}>
             <main>
               <Flex
-                w='100%'
-                mx='auto'
+                w="100%"
+                mx="auto"
                 px={6}
                 pb={6}
-                flexDirection={{ base: 'column', lg: 'row' }}
+                flexDirection={{ base: "column", lg: "row" }}
               >
                 {/* クレーム件数エリア */}
-                <Box w={{ base: '100%', lg: '800px' }} mx='auto' flex={'1'}>
+                <Box w={{ base: "100%", lg: "800px" }} mx="auto" flex={"1"}>
                   {claimCount() && (
                     <Box
-                      width='100%'
-                      boxShadow='xs'
-                      mt='6'
-                      p='6'
-                      rounded='md'
-                      bg='white'
+                      width="100%"
+                      boxShadow="xs"
+                      mt="6"
+                      p="6"
+                      rounded="md"
+                      bg="white"
                     >
-                      <Text fontSize='md' mt='1' ml='1'>
+                      <Text fontSize="md" mt="1" ml="1">
                         クレーム報告書 未処理件数：
-                        <Box as='span' color='red' fontWeight='bold'>
+                        <Box as="span" color="red" fontWeight="bold">
                           {claimCount()}
-                        </Box>{' '}
+                        </Box>{" "}
                         件
                         <Box>
                           ※「Menu」にある
-                          <Link href='/claims'>
+                          <Link href="/claims">
                             <a>
                               <Text
-                                as='span'
-                                textDecoration='underline'
+                                as="span"
+                                textDecoration="underline"
                                 _hover={{
-                                  textDecoration: 'none',
+                                  textDecoration: "none",
                                 }}
                               >
                                 クレーム報告書一覧
@@ -265,37 +283,37 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
                 </Box>
                 <Box
                   // w={{ base: '100%', md: '800px' }}
-                  w='100%'
-                  mt='6'
-                  mx='auto'
-                  ml={{ base: '0', lg: '6' }}
-                  p='6'
-                  rounded='md'
-                  boxShadow='xs'
-                  bg='white'
-                  flex='1'
-                  borderRadius={'lg'}
+                  w="100%"
+                  mt="6"
+                  mx="auto"
+                  ml={{ base: "0", lg: "6" }}
+                  p="6"
+                  rounded="md"
+                  boxShadow="xs"
+                  bg="white"
+                  flex="1"
+                  borderRadius={"lg"}
                 >
                   <Flex
-                    flexDirection={{ base: 'column', lg: 'row' }}
-                    alignItems={'center'}
-                    mt='1'
-                    mb='2'
+                    flexDirection={{ base: "column", lg: "row" }}
+                    alignItems={"center"}
+                    mt="1"
+                    mb="2"
                   >
-                    <Text fontSize='2xl' mb='2' mr='3'>
+                    <Text fontSize="2xl" mb="2" mr="3">
                       お手伝い依頼一覧
                     </Text>
                     <Tabs
-                      size='sm'
-                      variant='soft-rounded'
-                      colorScheme='gray'
-                      mb='2'
+                      size="sm"
+                      variant="soft-rounded"
+                      colorScheme="gray"
+                      mb="2"
                     >
                       <TabList>
-                        <Tab onClick={isDisplay} _focus={{ outline: 'none' }}>
+                        <Tab onClick={isDisplay} _focus={{ outline: "none" }}>
                           掲載中
                         </Tab>
-                        <Tab onClick={isHide} _focus={{ outline: 'none' }}>
+                        <Tab onClick={isHide} _focus={{ outline: "none" }}>
                           掲載終了
                         </Tab>
                       </TabList>
@@ -319,10 +337,10 @@ const Home: NextPage<any> = ({ sloganData, newsData, linkData }) => {
 export default Home;
 
 export async function getStaticProps() {
-  const accessPoint = 'https://portal-site.microcms.io/api/v1';
+  const accessPoint = "https://portal-site.microcms.io/api/v1";
   const options = {
     headers: {
-      'X-MICROCMS-API-KEY': '5c23d3e8eaa0448388ca527e0e00c829611f',
+      "X-MICROCMS-API-KEY": "5c23d3e8eaa0448388ca527e0e00c829611f",
     },
   };
   const sloganRes = await fetch(`${accessPoint}/slogan`, options);
