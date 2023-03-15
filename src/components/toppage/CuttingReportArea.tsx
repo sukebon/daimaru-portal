b35da997a1baeaae860a61ec82cbf6ea1ad54c41
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-03-15 11:23:25 +0900

    cutting

---
+++ src/components/toppage/CuttingReportArea.tsx
@@ -0,0 +1,68 @@
+import {
+  Box,
+  Flex,
+  List,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import useSWR from "swr";
+import { CuttingReportType } from "../../../types/CuttingReportType";
+
+const CuttingReportArea = () => {
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+  const { data } = useSWR("/api/cutting-reports/", fetcher);
+  console.log(data);
+  return (
+    <>
+      {data?.contents?.length > 0 && (
+        <Box
+          width="100%"
+          boxShadow="xs"
+          p={{ base: 3, md: 6 }}
+          rounded="md"
+          bg="white"
+        >
+          <Text fontSize="2xl" mb="4" ml="1">
+            本日の裁断報告書
+          </Text>
+
+          <TableContainer>
+            <Table variant="simple" size="sm">
+              <Thead>
+                <Tr>
+                  <Th>加工指示書NO.</Th>
+                  <Th>受注先名</Th>
+                  <Th>品名</Th>
+                  <Th>数量</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data?.contents?.map((value: CuttingReportType) => (
+                  <>
+                    <Tr>
+                      <Td>{value?.processNumber}</Td>
+                      <Td>{value?.client}</Td>
+                      <Td>{value?.itemName}</Td>
+                      <Td>{value?.totalQuantity}</Td>
+                    </Tr>
+                  </>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default CuttingReportArea;
---
+++ src/pages/api/cutting-reports.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { CuttingReportType } from "../../../types/CuttingReportType";
+type Data = {
+  contents: CuttingReportType[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const url = "https://daimaru-kijizaiko.vercel.app/api/cutting-reports/";
+  const contents = await axios
+    .get(url, {
+      params: { API_KEY: "daimaru-kijizaiko" },
+    })
+    .then((res) => res.data);
+  res.status(200).json({ contents: contents.contents });
+}
