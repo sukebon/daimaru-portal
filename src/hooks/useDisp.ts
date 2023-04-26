22fbf72a474d99c921d0979b672c4e7e594b4365
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-04-27 08:57:22 +0900

    claim

--- src/hooks/useDisp.ts
+++ src/hooks/useDisp.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from "../../store/useAuthStore";
 export const useDisp = () => {
   const users = useAuthStore((state) => state.users);
 
-  const getUserName = (uid: string) => {
+  const getUserName = (uid: string = "") => {
     const userName = users.find((user: User) => {
       if (user.uid === uid) return true;
     });
