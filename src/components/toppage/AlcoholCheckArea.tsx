bfc98fc294b397c5cf0e786904d186797334ff93
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-10-14 11:31:53 +0900

    alcoholcheck

--- src/components/toppage/AlcoholCheckArea.tsx
+++ src/components/toppage/AlcoholCheckArea.tsx
@@ -4,68 +4,28 @@ import {
   Button,
   Flex,
   Modal,
-  ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Radio,
-  RadioGroup,
-  Stack,
   useDisclosure,
 } from "@chakra-ui/react";
-import {
-  addDoc,
-  arrayUnion,
-  collection,
-  doc,
-  getDoc,
-  onSnapshot,
-  serverTimestamp,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import React, { FC, useEffect, useState } from "react";
 import { db } from "../../../firebase";
 import { useUtils } from "../../hooks/useUtils";
 import { useAuthStore } from "../../../store/useAuthStore";
+import { AlcoholCheckForm } from "../alcohol-checker/AlcoholCheckForm";
 
 export const AlcoholCheckArea: FC = () => {
   const currentUser = useAuthStore((state) => state.currentUser);
-  const [alcoholCheck1, setAlcoholCheck1] = useState("1");
-  const [alcoholCheck2, setAlcoholCheck2] = useState("1");
   const [flag, setFlag] = useState(false);
   const { todayDate } = useUtils();
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  //アルコールチェック登録
-  const setAlcoholCheckList = async () => {
-    try {
-      const docListRef = doc(db, "alcoholCheckList", `${todayDate()}`);
-      const docSnap = await getDoc(docListRef);
-
-      if (docSnap.exists()) {
-        await updateDoc(docListRef, {
-          member: arrayUnion(currentUser),
-        });
-      } else {
-        await setDoc(docListRef, {
-          id: `${todayDate()}`,
-          member: arrayUnion(currentUser),
-        });
-      }
-
-      await addDoc(collection(db, "alcoholCheckData"), {
-        date: `${todayDate()}`,
-        uid: currentUser,
-        createdAt: serverTimestamp(),
-        alcoholCheck1,
-        alcoholCheck2,
-      });
-    } catch (e) {
-      console.error(e);
-    }
+  const defaultValues = {
+    alcoholCheck1: "1",
+    alcoholCheck2: "1",
+    alcoholCheckValue: 0,
   };
 
   useEffect(() => {
@@ -101,57 +61,11 @@ export const AlcoholCheckArea: FC = () => {
         <ModalContent>
           <ModalHeader>アルコールチェック</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <>
-              <Box>
-                <Box>アルコールの検査はしましたか？</Box>
-                <RadioGroup
-                  defaultValue="2"
-                  value={alcoholCheck1}
-                  onChange={(e) => setAlcoholCheck1(e)}
-                >
-                  <Stack spacing={8} direction="row" mt={1}>
-                    <Radio colorScheme="red" value="0">
-                      No
-                    </Radio>
-                    <Radio colorScheme="green" value="1">
-                      Yes
-                    </Radio>
-                  </Stack>
-                </RadioGroup>
-              </Box>
-              <Box mt={3}>
-                <Box>酒気帯び</Box>
-                <RadioGroup
-                  defaultValue="2"
-                  mt={1}
-                  value={alcoholCheck2}
-                  onChange={(e) => setAlcoholCheck2(e)}
-                >
-                  <Stack spacing={9} direction="row">
-                    <Radio colorScheme="red" value="0">
-                      有
-                    </Radio>
-                    <Radio colorScheme="green" value="1">
-                      無
-                    </Radio>
-                  </Stack>
-                </RadioGroup>
-              </Box>
-            </>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              width="100px"
-              colorScheme="facebook"
-              onClick={() => {
-                setAlcoholCheckList();
-                onClose();
-              }}
-            >
-              提出
-            </Button>
-          </ModalFooter>
+          <AlcoholCheckForm
+            onClose={onClose}
+            pageType="NEW"
+            defaultValues={defaultValues}
+          />
         </ModalContent>
       </Modal>
     </>
---
+++ src/pages/alcohol-checker/EditAlcoholCheck.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { AlcoholCheckForm } from "@/components/alcohol-checker/AlcoholCheckForm";
+import {BiSolidEditAlt} from "react-icons/bi"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+
+type Inputs = {
+  alcoholCheck1: string;
+  alcoholCheck2: string;
+  alcoholCheckValue: number;
+};
+
+type Props = {
+    postId: string;
+  defaultValues: Inputs;
+};
+
+export const EditAlcoholCheck: FC<Props> = ({ postId, defaultValues }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <BiSolidEditAlt fontSize={19} onClick={onOpen} cursor="pointer" />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <AlcoholCheckForm
+            onClose={onClose}
+            pageType="EDIT"
+            postId={postId}
+            defaultValues={defaultValues}
+          />
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
