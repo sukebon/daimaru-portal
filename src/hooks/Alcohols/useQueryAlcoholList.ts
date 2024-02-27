594135911c286f2afbb726fb695ef22362e509d5
daimaru-hakui <dh.mukai.dh@gmail.com>
2024-02-27 12:07:50 +0900

    アルコールチェック　一覧変更

--- src/hooks/Alcohols/useQueryAlcoholList.ts
+++ src/hooks/Alcohols/useQueryAlcoholList.ts
@@ -1,22 +1,34 @@
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
-import { db } from '../../../firebase';
-import { AlcoholCheckList } from '../../../types';
-import { useQuery } from '@tanstack/react-query';
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAt,
+} from "firebase/firestore";
+import { db } from "../../../firebase";
+import { AlcoholCheckList } from "../../../types";
+import { useQuery } from "@tanstack/react-query";
 
-export const useQueryAlcoholList = (count: number = 10) => {
-  
-  const getAlcoholCheckList = async() => {
-    const collectionRef = collection(db, 'alcoholCheckList')
-    const q = query(collectionRef, orderBy("id", "desc"), limit(count));
-    const snapshot = await getDocs(q)
-    const data: AlcoholCheckList[] = snapshot.docs.map((doc) => (
-      { ...doc.data(), id:doc.id } as AlcoholCheckList
-    ))
-    return data
-  }
-  
+export const useQueryAlcoholList = (
+  count: number = 30,
+) => {
+  const getAlcoholCheckList = async () => {
+    const collectionRef = collection(db, "alcoholCheckList");
+    const q = query(
+      collectionRef,
+      orderBy("id", "desc"),
+      limit(count)
+    );
+    const snapshot = await getDocs(q);
+    const data: AlcoholCheckList[] = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id } as AlcoholCheckList)
+    );
+    return data;
+  };
+
   return useQuery({
-    queryKey:['alcoholCheckList'],
+    queryKey: ["alcoholCheckList"],
     queryFn: getAlcoholCheckList,
-  })
-}
+  });
+};
